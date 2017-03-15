cf92381ead51082304c85444bfa2526530d71cd1
Leon Xu <xfstart07@gmail.com>
2017-03-15 14:09:52 +0800

    修改es6 语法支持

---
+++ src/components/first.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="first">
+    <h1>I am a title.</h1>
+    <a href="#">written by {{ author }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      author: 'Leon Xu'
+    }
+  }
+}
+</script>
